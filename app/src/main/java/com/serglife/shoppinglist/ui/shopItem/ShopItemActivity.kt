ccd9f815ca16705e6b3bac6b4f7d9ace73aafd26
Sergey <serglife777@gmail.com>
2021-09-15 02:20:18 +0300

    add activity item shop

--- app/src/main/java/com/serglife/shoppinglist/ui/shopItem/ShopItemActivity.kt
+++ app/src/main/java/com/serglife/shoppinglist/ui/shopItem/ShopItemActivity.kt
@@ -2,6 +2,7 @@ package com.serglife.shoppinglist.ui.shopItem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.serglife.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
